5ac2e41b97301c17a890c591c14e0720378a8a39
cha1ra <cha1ra.garnet@gmail.com>
2024-08-13 01:16:00 +0900

    feat: 自動デプロイ

---
+++ app/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p&family=Noto+Serif+JP&display=swap" rel="stylesheet">
+    <title>しゃべクリップ</title>
+</head>
+<body class="bg-gray-100 h-screen">
+    <div class="container mx-auto mt-8 p-4 flex flex-col items-center h-screen">
+        <img src="./assets/images/logo-text.png" alt="しゃべクリップ" class="w-1/2">
+        <p>Under Construction...</p>
+    </div>
+</body>
+</html>
